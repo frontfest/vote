be3cb76586f54dd4d902123ab9eaaf268d32b6cb
luisddm <luisddm@posteo.net>
2018-11-21 18:17:59 +0100

    es2015-flavored express.js boilerplate

---
+++ README.md
@@ -0,0 +1,6 @@
+# Webapp de FrontFest para hacer votaciones
+
+```bash
+yarn install
+yarn start
+```
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+
+const router = Router()
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
+
+export default router
